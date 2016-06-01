eaa0c898a27e33ef370d8cd9a6243b5ade9a0a0c
Dmitriy Varygin <dmitriy.varygin@yandex.ru>
2016-06-01 19:01:19 +0500

    Worked on integration test

---
+++ src/test/java/ru/dvvar/topjava/desktop/integration/IntegrationTests.java
@@ -0,0 +1,29 @@
+package ru.dvvar.topjava.desktop.integration;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.dvvar.topjava.desktop.config.Config;
+import ru.dvvar.topjava.desktop.controller.Controller;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * Created by Dmitriy_Varygin on 01.06.2016.
+ */
+public class IntegrationTests {
+
+    @Test
+    public void shouldAllWorkAndPrintOneUserMealWithExceed() throws Exception {
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        Controller controller = context.getBean(Controller.class);
+        ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(arrayOutputStream));
+        controller.getAll();
+        String result = arrayOutputStream.toString();
+        TestCase.assertTrue(result.contains("UserMealWithExceed") && result.contains("id")
+                && result.contains("description") && result.contains("dateTime") && result.contains("exceed"));
+    }
+}
